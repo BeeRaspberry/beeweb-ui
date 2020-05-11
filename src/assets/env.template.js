f6b429240736d73b3a75dc4d88d7a57ec24b2a79
erikdeirdre <pwhite@delpwhite.org>
2020-05-11 17:57:00 -0400

    changed wrong file

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -2,6 +2,6 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["apiUrl"] = "${API_URL}";
+  window["env"]["apiUrl"] = '"${API_URL}"';
   window["env"]["debug"] = true;
 })(this);
